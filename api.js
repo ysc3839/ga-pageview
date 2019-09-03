a43102940288accf4e1b1b38c6c6387d110f18a0
Richard Yu <yurichard3839@gmail.com>
2019-09-03 15:26:58 +0800

    Move config out

--- api.js
+++ api.js
@@ -6,89 +6,90 @@
 const {URL} = require('url');
 const {google} = require('googleapis');
 const analyticsreporting = google.analyticsreporting({version: 'v4'});
-const config = require('./config.json');
 
 const auth = new google.auth.GoogleAuth({
   scopes: ['https://www.googleapis.com/auth/analytics.readonly'],
 });
 
-const reqParams = {
-  viewId: config.viewId,
-  dateRanges: [{
-    startDate: '2005-01-01',
-    endDate: '9999-12-31',
-  }],
-  samplingLevel: 'LARGE',
-  hideTotals: true,
-  hideValueRanges: true,
-};
+module.exports = function(config) {
+  const reqParams = {
+    viewId: config.viewId,
+    dateRanges: [{
+      startDate: '2005-01-01',
+      endDate: '9999-12-31',
+    }],
+    samplingLevel: 'LARGE',
+    hideTotals: true,
+    hideValueRanges: true,
+  };
 
-/**
- * @param {request} req
- * @param {response} res
- */
-module.exports = async function(req, res) {
-  let path;
-  try {
-    const url = new URL(req.header('referer'));
+  /**
+   * @param {request} req
+   * @param {response} res
+   */
+  return async function(req, res) {
+    let path;
+    try {
+      const url = new URL(req.header('referer'));
 
-    let pass = false;
-    if (typeof config.refererHost === 'string') {
-      pass = (url.hostname === config.refererHost);
-    } else if (config.refererHost instanceof Array) {
-      pass = config.refererHost.includes(url.hostname);
-    }
-
-    if (pass) {
-      path = url.pathname;
-      if (config.defaultPage && path[path.length - 1] === '/') {
-        path += config.defaultPage;
+      let pass = false;
+      if (typeof config.refererHost === 'string') {
+        pass = (url.hostname === config.refererHost);
+      } else if (config.refererHost instanceof Array) {
+        pass = config.refererHost.includes(url.hostname);
       }
-    }
-  } catch (e) {}
-  if (!path) {
-    res.status(400).end();
-    return;
-  }
 
-  try {
-    const report = await analyticsreporting.reports.batchGet({
-      auth: auth,
-      requestBody: {
-        reportRequests: [
-          {
-            metrics: [
-              {expression: 'ga:pageviews'},
-              {expression: 'ga:uniquePageviews'},
-            ],
-            ...reqParams,
-          },
-          {
-            dimensionFilterClauses: [{
-              filters: [{
-                dimensionName: 'ga:pagePath',
-                operator: 'BEGINS_WITH',
-                expressions: [path],
-                caseSensitive: true,
+      if (pass) {
+        path = url.pathname;
+        if (config.defaultPage && path[path.length - 1] === '/') {
+          path += config.defaultPage;
+        }
+      }
+    } catch (e) {}
+    if (!path) {
+      res.status(400).end();
+      return;
+    }
+
+    try {
+      const report = await analyticsreporting.reports.batchGet({
+        auth: auth,
+        requestBody: {
+          reportRequests: [
+            {
+              metrics: [
+                {expression: 'ga:pageviews'},
+                {expression: 'ga:uniquePageviews'},
+              ],
+              ...reqParams,
+            },
+            {
+              dimensionFilterClauses: [{
+                filters: [{
+                  dimensionName: 'ga:pagePath',
+                  operator: 'BEGINS_WITH',
+                  expressions: [path],
+                  caseSensitive: true,
+                }],
               }],
-            }],
-            metrics: [{expression: 'ga:pageviews'}],
-            ...reqParams,
-          },
-        ],
-      },
-    });
-    const [sitePv, siteUv] =
+              metrics: [{expression: 'ga:pageviews'}],
+              ...reqParams,
+            },
+          ],
+        },
+      });
+      const [sitePv, siteUv] =
       report.data.reports[0].data.rows[0].metrics[0].values.map(Number);
-    const pagePv =
+      const pagePv =
       Number(report.data.reports[1].data.rows[0].metrics[0].values[0]);
-    res.jsonp({
-      site_uv: siteUv,
-      page_pv: pagePv,
-      site_pv: sitePv,
-    });
-  } catch (e) {
-    res.status(500).end();
-    console.log(e);
-  }
+      res.jsonp({
+        site_uv: siteUv,
+        page_pv: pagePv,
+        site_pv: sitePv,
+      });
+    } catch (e) {
+      res.status(500).end();
+      console.log(e);
+    }
+  };
 };
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const api = require('./api');
+const config = require('./config.json');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/api', api);
+app.get('/api', api(config));
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
